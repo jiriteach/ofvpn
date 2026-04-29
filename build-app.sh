bb4b4db21d02f44cb6c272862a14acbe2b80fa9f
Jithen (J) Singh <jxs@s7n.dev>
2026-04-29 16:31:28 +1200

    Fixes

--- build-app.sh
+++ build-app.sh
@@ -47,9 +47,9 @@ cat > "${CONTENTS_DIR}/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.28</string>
+    <string>1.30</string>
     <key>CFBundleVersion</key>
-    <string>28</string>
+    <string>30</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>LSUIElement</key>
